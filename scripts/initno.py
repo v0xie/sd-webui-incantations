045056699c3782da2cd377dc60e657a575d2160e
v0xie <28695009+v0xie@users.noreply.github.com>
2024-05-22 10:38:45 -0700

    blocking out

--- scripts/initno.py
+++ scripts/initno.py
@@ -1,9 +1,23 @@
 import gradio as gr
-from scripts.ui_wrapper import UIWrapper
-from modules.processing import StableDiffusionProcessing
-from modules import script_callbacks
-from modules.script_callbacks import CFGDenoiserParams, CFGDenoisedParams, AfterCFGCallbackParams
 import torch
+import logging
+from scripts.ui_wrapper import UIWrapper
+from modules import shared, script_callbacks
+from modules.processing import StableDiffusionProcessing
+from modules.script_callbacks import CFGDenoiserParams, CFGDenoisedParams, AfterCFGCallbackParams
+from modules.sd_samplers_cfg_denoiser import catenate_conds
+
+from scripts.incant_utils import module_hooks
+
+logger = logging.getLogger(__name__)
+
+class InitnoParams():
+    def __init__(self):
+        self.x_in = None
+        self.sigma_in = None
+        self.image_cond_in = None
+        self.text_cond = None
+        self.text_uncond = None
 
 class InitnoScript(UIWrapper):
     def __init__(self):
@@ -33,33 +47,122 @@ class InitnoScript(UIWrapper):
 
     def before_process_batch(self, p, *args, **kwargs):
         self.unhook_callbacks()
+        active = getattr(p, 'embeds_active', active)
+        if not active:
+            return
+        
+        def initno_cross_attn_hook(module, input, kwargs, output):
+            setattr(module, 'initno_crossattn', output.detach().clone())
+            pass
+
+        def initno_self_attn_hook(module, input, kwargs, output):
+            setattr(module, 'initno_selfattn', output.detach().clone())
+            pass
+
+        crossattn_modules = self.get_all_crossattn_modules()
+        for module in crossattn_modules:
+            module_hooks.module_add_forward_hook(module, initno_cross_attn_hook, hook_type='forward', with_kwargs=True)
+            module_hooks.module_add_forward_hook(module, initno_self_attn_hook, hook_type='forward', with_kwargs=True)
+            if module.network_layer_name.endswith('attn1'):
+                module_hooks.modules_add_field(module, 'initno_selfattn', None)
+            if module.network_layer_name.endswith('attn2'):
+                module_hooks.modules_add_field(module, 'initno_crossattn', None)
+        
+    def get_all_crossattn_modules(self):
+            """ 
+            Get ALL attention modules
+            """
+            modules = module_hooks.get_modules(
+                    module_name_filter='CrossAttention'
+            )
+            return modules
 
     def process_batch(self, p: StableDiffusionProcessing, active, *args, **kwargs):
         active = getattr(p, 'embeds_active', active)
         if not active:
             return
 
-        def on_cfg_denoiser(params: CFGDenoiserParams):
-            pass
-            
-        script_callbacks.on_cfg_denoiser(lambda x: on_cfg_denoiser(x))
+        initno_params= InitnoParams()
 
-    def optimize_noise(self, x):
+        def on_cfg_denoiser(params: CFGDenoiserParams, initno_params: InitnoParams):
+            if initno_params.x_in is None:
+                initno_params.x_in = params.x
+                initno_params.sigma_in = params.sigma
+                initno_params.image_cond_in = params.image_cond
+                initno_params.text_cond = params.text_cond
+                initno_params.text_uncond = params.text_uncond
+                logger.debug("Initializing initial_x with shape %s", params.x.shape)
+        
+        def on_cfg_denoised(params: CFGDenoisedParams, initno_params: InitnoParams):
+            x = params.x
+            tensor = initno_params.text_cond
+            uncond = initno_params.text_uncond
+            sigma_in = initno_params.sigma_in
+            image_cond_in = initno_params.image_cond_in
+
+            if initno_params.x_in is None:
+                return
+
+            logger.debug("Initial_x is not None, shape: %s", initno_params.x_in.shape)
+
+            # text_cond and uncond don't change
+            cond_in = catenate_conds([tensor, uncond])
+            make_condition_dict = get_make_condition_dict_fn(uncond)
+            conds = make_condition_dict(cond_in, image_cond_in)
+
+            max_step = 50
+            max_round = 5
+            noise_mean = torch.zeros_like(x)
+            noise_std = torch.ones_like(x)
+
+            optim = torch.optim.Adam([noise_mean, noise_std], lr=0.01)
+            for round in range(max_round):
+                for step in range(max_step):
+                        pass
+
+            
+        script_callbacks.on_cfg_denoiser(lambda params: on_cfg_denoiser(params, initno_params))
+        script_callbacks.on_cfg_denoised(lambda params: on_cfg_denoised(params, initno_params))
+
+    def optimize_noise(self, x, t_c=0.2, t_s=0.3):
         """ Optimize noise for a given image.
         Arguments:
             x: input initial latent
+            t_c: response score threshold for cross attn
+            t_s: response score threshold for self attn
         """
         max_step = 50
         max_round = 5
-        for step in range(max_step):
-            for round in range(max_round):
-                pass
+        noise_mean = torch.zeros_like(x)
+        noise_std = torch.ones_like(x)
+
+        optim = torch.optim.Adam([noise_mean, noise_std], lr=0.01)
+        for round in range(max_round):
+            for step in range(max_step):
+                    pass
 
     def postprocess_batch(self, p, *args, **kwargs):
         pass
     
     def unhook_callbacks(self) -> None:
         script_callbacks.remove_current_script_callbacks()
+        crossattn_modules = self.get_all_crossattn_modules()
+        for module in crossattn_modules:
+            module_hooks.remove_module_forward_hook(module, 'initno_cross_attn_hook')
+            module_hooks.remove_module_forward_hook(module, 'initno_self_attn_hook')
+            module_hooks.modules_remove_field(module, 'initno_selfattn')
+            module_hooks.modules_remove_field(module, 'initno_crossattn')
 
     def get_xyz_axis_options(self) -> dict:
         return {}
+
+# from modules/sd_samplers_cfg_denoiser.py:187-195
+def get_make_condition_dict_fn(text_uncond):
+        if shared.sd_model.model.conditioning_key == "crossattn-adm":
+                make_condition_dict = lambda c_crossattn, c_adm: {"c_crossattn": [c_crossattn], "c_adm": c_adm}
+        else:
+                if isinstance(text_uncond, dict):
+                        make_condition_dict = lambda c_crossattn, c_concat: {**c_crossattn, "c_concat": [c_concat]}
+                else:
+                        make_condition_dict = lambda c_crossattn, c_concat: {"c_crossattn": [c_crossattn], "c_concat": [c_concat]}
+        return make_condition_dict
