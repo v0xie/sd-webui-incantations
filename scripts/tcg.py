4bf6d3eaee349abe9ee830066a87b9d0fab66a12
v0xie <28695009+v0xie@users.noreply.github.com>
2024-05-25 13:34:01 -0700

    add scaffolding

--- scripts/tcg.py
+++ scripts/tcg.py
@@ -1,4 +1,6 @@
+import logging
 import gradio as gr
+import torch
 from scripts.ui_wrapper import UIWrapper
 from scripts.incant_utils import module_hooks
 
@@ -8,6 +10,9 @@ Author: v0xie
 GitHub URL: https://github.com/v0xie/sd-webui-incantations
 
 """
+
+logger = logging.getLogger(__name__)
+
 class TCGExtensionScript(UIWrapper):
     def __init__(self):
         self.infotext_fields: list = []
@@ -23,6 +28,26 @@ class TCGExtensionScript(UIWrapper):
             for opt in opts:
                 opt.do_not_save_to_config = True
             return opts
+    
+    def get_modules(self):
+        return module_hooks.get_modules( module_name_filter='CrossAttention')
+    
+    def hook_modules(self):
+        def tcg_to_q_hook(module, input, kwargs, output):
+                setattr(module.tcg_parent_module[0], 'tcg_to_q_map', output)
+
+        def tcg_to_k_hook(module, input, kwargs, output):
+                setattr(module.tcg_parent_module[0], 'tcg_to_k_map', output)
+
+        for module in self.get_modules():
+            if not module.network_layer_name.endswith('attn2'):
+                continue
+            module_hooks.modules_add_field(module, 'tcg_to_q_map', None)
+            module_hooks.modules_add_field(module, 'tcg_to_k_map', None)
+            module_hooks.modules_add_field(module.to_q, 'tcg_parent_module', [module])
+            module_hooks.modules_add_field(module.to_k, 'tcg_parent_module', [module])
+            module_hooks.module_add_forward_hook(module.to_q, tcg_to_q_hook, with_kwargs=True)
+            module_hooks.module_add_forward_hook(module.to_k, tcg_to_k_hook, with_kwargs=True)
 
     def before_process(self, p, *args, **kwargs):
         pass
@@ -30,21 +55,54 @@ class TCGExtensionScript(UIWrapper):
     def process(self, p, *args, **kwargs):
         pass
 
-    def before_process_batch(self, p, *args, **kwargs):
-        pass
+    def before_process_batch(self, p, active, *args, **kwargs):
+        active = getattr(p, 'tcg_active', active)
+        if not active:
+            return
 
     def process_batch(self, p, *args, **kwargs):
         pass
 
     def postprocess_batch(self, p, *args, **kwargs):
-        pass
+        self.unhook_callbacks()
     
     def unhook_callbacks(self) -> None:
-        pass
+        for module in self.get_modules():
+            module_hooks.remove_module_forward_hook(module.to_q, 'tcg_to_q_hook')
+            module_hooks.remove_module_forward_hook(module.to_k, 'tcg_to_k_hook')
+            module_hooks.modules_remove_field(module, 'tcg_to_q_map')
+            module_hooks.modules_remove_field(module, 'tcg_to_k_map')
+            module_hooks.modules_remove_field(module.to_q, 'tcg_parent_module')
+            module_hooks.modules_remove_field(module.to_k, 'tcg_parent_module')
 
     def get_xyz_axis_options(self) -> dict:
         return {}
 
-def arg(p, field_name: str, variable_name:str, default=None, **kwargs):
-        """ Get argument from field_name or variable_name, or default if not found """
-        return getattr(p, field_name, kwargs.get(variable_name, None))
+
+def get_attention_scores(to_q_map, to_k_map, dtype):
+        """ Calculate the attention scores for the given query and key maps
+        Arguments:
+                to_q_map: torch.Tensor - query map
+                to_k_map: torch.Tensor - key map
+                dtype: torch.dtype - data type of the tensor
+        Returns:
+                torch.Tensor - attention scores 
+        """
+        # based on diffusers models/attention.py "get_attention_scores"
+        # use in place operations vs. softmax to save memory: https://stackoverflow.com/questions/53732209/torch-in-place-operations-to-save-memory-softmax
+        # 512x: 2.65G -> 2.47G
+        # attn_probs = attn_scores.softmax(dim=-1).to(device=shared.device, dtype=to_q_map.dtype)
+
+        attn_probs = to_q_map @ to_k_map.transpose(-1, -2)
+
+        # avoid nan by converting to float32 and subtracting max 
+        attn_probs = attn_probs.to(dtype=torch.float32) #
+        attn_probs -= torch.max(attn_probs)
+
+        torch.exp(attn_probs, out = attn_probs)
+        summed = attn_probs.sum(dim=-1, keepdim=True, dtype=torch.float32)
+        attn_probs /= summed
+
+        attn_probs = attn_probs.to(dtype=dtype)
+
+        return attn_probs
