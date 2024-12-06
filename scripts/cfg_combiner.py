9fe8d5d4135fb5d7f365828c3c9c982b575550fb
v0xie <28695009+v0xie@users.noreply.github.com>
2024-12-06 15:57:08 -0800

    add CFGCombinerParams with current step field, reorder OOP

--- scripts/cfg_combiner.py
+++ scripts/cfg_combiner.py
@@ -12,6 +12,12 @@ from scripts.adaptive_projected_guidance import normalized_guidance
 
 logger = logging.getLogger(__name__)
 
+
+class CFGCombinerParams:
+        def __init__(self):
+                self.current_step = 0
+
+
 class CFGCombinerScript(UIWrapper):
         """ Some scripts modify the CFGs in ways that are not compatible with each other.
             This script will patch the CFG denoiser function to apply CFG in an ordered way.
@@ -72,8 +78,8 @@ class CFGCombinerScript(UIWrapper):
                 return
 
             #logger.debug("CFGCombinerScript process_batch: pag_active or scfg_active")
-
-            cfg_denoise_lambda = lambda params: self.on_cfg_denoiser_callback(params, p.incant_cfg_params)
+            cfg_params = CFGCombinerParams()
+            cfg_denoise_lambda = lambda params: self.on_cfg_denoiser_callback(params, p.incant_cfg_params, cfg_params)
             unhook_lambda = lambda: self.unhook_callbacks()
 
             script_callbacks.on_cfg_denoiser(cfg_denoise_lambda)
@@ -89,17 +95,19 @@ class CFGCombinerScript(UIWrapper):
                     return
             self.unpatch_cfg_denoiser(cfg_dict)
 
-        def on_cfg_denoiser_callback(self, params: CFGDenoiserParams, cfg_dict: dict):
+        def on_cfg_denoiser_callback(self, params: CFGDenoiserParams, cfg_dict: dict, cfg_params: CFGCombinerParams):
             """ Callback for when the CFG denoiser is called 
             Patches the combine_denoised function with a custom one.
             """
+            cfg_params.current_step = params.sampling_step
+
             if cfg_dict['denoiser'] is None:
                     cfg_dict['denoiser'] = params.denoiser
             else:
                     self.unpatch_cfg_denoiser(cfg_dict)
-            self.patch_cfg_denoiser(params.denoiser, cfg_dict)
+            self.patch_cfg_denoiser(params.denoiser, cfg_dict, cfg_params)
 
-        def patch_cfg_denoiser(self, denoiser, cfg_dict: dict):
+        def patch_cfg_denoiser(self, denoiser, cfg_dict: dict, cfg_params: CFGCombinerParams):
             """ Patch the CFG Denoiser combine_denoised function """
             if not cfg_dict:
                     logger.error("Unable to patch CFG Denoiser, no dict passed as cfg_dict")
@@ -107,7 +115,9 @@ class CFGCombinerScript(UIWrapper):
             if not denoiser:
                     logger.error("Unable to patch CFG Denoiser, denoiser is None")
                     return
-
+            if not cfg_params:
+                    logger.error("Unable to patch CFG Denoiser, cfg_params is None")
+                    return
             if getattr(denoiser, 'combine_denoised_patched', False) is False:
                     try:
                             setattr(denoiser, 'combine_denoised_original', denoiser.combine_denoised)
@@ -115,6 +125,7 @@ class CFGCombinerScript(UIWrapper):
                             pass_conds_func = lambda *args, **kwargs: combine_denoised_pass_conds_list(
                                     *args,
                                     **kwargs,
+                                    cfg_params = cfg_params,
                                     original_func = denoiser.combine_denoised_original,
                                     pag_params = cfg_dict['pag_params'],
                                     scfg_params = cfg_dict['scfg_params'],
@@ -161,10 +172,15 @@ def combine_denoised_pass_conds_list(*args, **kwargs):
             3. ...
             ...
         """
+        cfg_params = kwargs.get('cfg_params', None)
         original_func = kwargs.get('original_func', None)
         pag_params = kwargs.get('pag_params', None)
         scfg_params = kwargs.get('scfg_params', None)
         apg_params = kwargs.get('apg_params', None)
+
+        if cfg_params is None:
+                logger.error("No CFGCombinerParams passed to combine_denoised_pass_conds_list")
+                return original_func(*args)
 
         if pag_params is None and scfg_params is None and apg_params is None:
                 logger.warning("No reason to hijack combine_denoised")
@@ -234,26 +250,17 @@ def combine_denoised_pass_conds_list(*args, **kwargs):
                                                # rate is tensor, probably
                                                pass
 
-                                # 1. Experimental formulation for S-CFG combined with CFG
-                                # dt = d0(z,t,y) - d0(z,t,null)
-                                # d_cfg = d0(z,t,null) + w(d0(z,t,y) - d0(z,t,null))
-                                # d_cfg = d0(z,t,null) + w(dt)
-
-
+                                # 1. Experimental formulation for S-CFG combined with CFG combined with APG
+                                cfg_x = (model_delta) * rate * (weight * cfg_scale)
 
                                 if apg_params is not None:
-                                        normalized_cond = normalized_guidance(
-                                               pred_cond=x_out[cond_index],
-                                               pred_uncond=denoised_uncond[i],
-                                               #diff = cfg_x,
-                                               guidance_scale = cfg_scale,
-                                               momentum_buffer = apg_params.momentum_buffer,
-                                               eta = apg_params.apg_parallel_scale,
-                                               norm_threshold = apg_params.norm_threshold,
-                                        )
-                                        cfg_x = normalized_cond * rate * (weight * (cfg_scale - 1))
-                                else:
-                                        cfg_x = (model_delta) * rate * (weight * cfg_scale)
+                                        if apg_params.apg_start_step <= cfg_params.current_step <= apg_params.apg_end_step:
+                                                normalized_cond = normalized_guidance(
+                                                        pred_cond=x_out[cond_index],
+                                                        pred_uncond=denoised_uncond[i],
+                                                        apg_params = apg_params,
+                                                )
+                                                cfg_x = normalized_cond * rate * (weight * (cfg_scale - 1))
 
                                 if not use_saliency_map or not run_pag:
                                         denoised[i] += cfg_x
